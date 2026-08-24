b27906c2211ffb94387d1dc880e6a1ff2c36e0eb
n00d46yo <carmenalsey@mail.com>
2026-08-24 18:35:20 +0700

    Support embed and direct video URLs

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,6 +1,54 @@
 <script setup lang="ts">
 import Hls from 'hls.js'
-const props=defineProps<{src:string}>(); const video=ref<HTMLVideoElement|null>(null)
-onMounted(()=>{ if(!video.value) return; if(video.value.canPlayType('application/vnd.apple.mpegurl')) video.value.src=props.src; else if(Hls.isSupported()){const h=new Hls();h.loadSource(props.src);h.attachMedia(video.value)} })
+
+const props = defineProps<{ src: string }>()
+
+const video = ref<HTMLVideoElement | null>(null)
+
+const isEmbed = computed(() => {
+  return /\/embed\//i.test(props.src)
+})
+
+onMounted(() => {
+  if (isEmbed.value) return
+
+  if (!video.value) return
+
+  const isHls =
+    /\.m3u8(\?|$)/i.test(props.src) ||
+    props.src.includes('.m3u8')
+
+  if (isHls) {
+    if (video.value.canPlayType('application/vnd.apple.mpegurl')) {
+      video.value.src = props.src
+    } else if (Hls.isSupported()) {
+      const h = new Hls()
+      h.loadSource(props.src)
+      h.attachMedia(video.value)
+    }
+  } else {
+    video.value.src = props.src
+  }
+})
 </script>
-<template><div class="overflow-hidden rounded-xl bg-black"><video ref="video" controls playsinline class="aspect-video w-full"/></div></template>
+
+<template>
+  <div class="overflow-hidden rounded-xl bg-black">
+    <iframe
+      v-if="isEmbed"
+      :src="props.src"
+      class="aspect-video w-full"
+      allow="autoplay; fullscreen; picture-in-picture"
+      allowfullscreen
+      frameborder="0"
+    />
+
+    <video
+      v-else
+      ref="video"
+      controls
+      playsinline
+      class="aspect-video w-full"
+    />
+  </div>
+</template>
